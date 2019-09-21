019ed86856d4e859bd96246cb5ce490c10b00dbd
Scott Goodrich <scottgoodrich516@gmail.com>
2019-09-21 13:52:16 -0700

    Dropdowns not functioning yet

--- server.js
+++ server.js
@@ -7,10 +7,10 @@ var PORT = process.env.PORT || 8080;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-require("app/routing/apiRoutes")(app);
-require("app/routing/htmlRoutes")(app);
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
 
 app.listen(PORT, function() {
-    console.log("App listening on PORT: " + PORT);
+    console.log("Server listening on: http://localhost:" + PORT);
   });
   
